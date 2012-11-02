e1c2bf4854968f06d954c67399a53f24c18a0936
Javier Trejo <jtrejo@cominteractiva.com.ar>
2012-11-02 17:46:03 -0300

    v0.1.0 complete

--- development/jquery-afv-0.1.0.js
+++ development/jquery-afv-0.1.0.js
@@ -13,15 +13,35 @@
  *
  */
 (function($) {
-	var defaultOptions = {
-		namespace : 'afv',
-		expresions:	{
+	var errorsCollection;
+	var defaultOptions 		= {
+		// Espacio de nombres por default para los atributos data-*
+		namespace 		: 'afv',
+		
+		// Expresiones regulares por default para la validación de formato
+		expresions		: {
 			email			: /^[A-Za-z0-9._%+-]+@([A-Za-z0-9-]+\.)+([A-Za-z0-9]{2,4}|museum)$/,
 			url				: /((http\:\/\/|https\:\/\/|ftp\:\/\/)|(www.))+(([a-zA-Z0-9\.-]+\.[a-zA-Z]{2,4})|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(\/[a-zA-Z0-9%:\/-_\?\.'~]*)?/,
 			string			: /^[a-zA-Z]+$/,
 			number			: /^\s*\d+\s*$/,
 			extendedString	: /^[-\sa-zA-Z0-9]+$/
-		}
+		},
+		
+		// Mensajes de error por default para mostrar al usuario
+		errorMessages	: {
+			required		: "El campo es obligatorio",
+			minLength		: "El campo debe tener {0} como mínimo",
+			maxLength		: "El campo debe tener {0} como máximo",
+			defaultOption	: "Debe seleccionar un valor",
+			equalTo			: "Los campos {0} y {1} deben ser iguales",
+			format			: "El valor ingresado no se corresponde con el formato requerido"
+		},
+		
+		// Indica la forma en que se muestran los mensajes de error
+		errorDisplay		: "inside",
+		
+		// Clase CSS por default para los campos con error
+		errorClass			: "afv-field-error"
 	};
 
 	var methods = {
@@ -35,31 +55,60 @@
 			});
 		},
 		validate: function(form) {
-			var isValid	= true;
+			var isValid			= true;
 			
+			errorsCollection	= [];
+			
+			$("." + defaultOptions.errorClass).removeClass(defaultOptions.errorClass).removeAttr("title");
+			
+			// Controla la propiedad campo obligatorio
 			form.find("[data-" + defaultOptions.namespace + "-required]").each(function(index, item){
 				var item	= $(item);
 				
 				if(item.val().length == 0)
+				{
 					isValid	= false;
+					errorsCollection.push({
+						field	: item,
+						message	: defaultOptions.errorMessages.required,
+						error	: $.ambarFormsValidator.Validator.REQUIRED
+					});	
+				}
 			});
 			
+			// Controla la propiedad cantidad mínima de caracteres de un campo
 			form.find("[data-" + defaultOptions.namespace + "-length-min]").each(function(index, item){
 				var item	= $(item);
 				var length	= $(item).attr("data-" + defaultOptions.namespace + "-length-min");
 				
 				if(item.val().length < length)
+				{
 					isValid	= false;
+					errorsCollection.push({
+						field	: item,
+						message	: defaultOptions.errorMessages.minLength,
+						error	: $.ambarFormsValidator.Validator.LENGTH_MIN
+					});	
+				}
 			});
 			
+			// Controla la propiedad cantidad máxima de caracteres de un campo
 			form.find("[data-" + defaultOptions.namespace + "-length-max]").each(function(index, item){
 				var item	= $(item);
 				var length	= $(item).attr("data-" + defaultOptions.namespace + "-length-max");
 				
 				if(item.val().length > length)
+				{
 					isValid	= false;
+					errorsCollection.push({
+						field	: item,
+						message	: defaultOptions.errorMessages.maxLength,
+						error	: $.ambarFormsValidator.Validator.LENGTH_MAX
+					});	
+				}
 			});
-			
+
+			// Controla la propiedad "igual a". Verifica si dos campos son iguales			
 			form.find("[data-" + defaultOptions.namespace + "-equal-to]").each(function(index, item){
 				var item	= $(item);
 				var target	= $(item).attr("data-" + defaultOptions.namespace + "-equal-to");
@@ -67,24 +116,47 @@
 				target		= $("#" + target);
 				
 				if(item.val() != target.val())
+				{
 					isValid	= false;
+					errorsCollection.push({
+						field	: item,
+						message	: defaultOptions.errorMessages.equalTo,
+						error	: $.ambarFormsValidator.Validator.EQUAL_TO
+					});	
+				}
 			});
 			
+			// Controla que el usuario haya seleccionado un valor en un campo select a través del valor por default
 			form.find("[data-" + defaultOptions.namespace + "-default]").each(function(index, item){
 				var item	= $(item);
 				var valDef	= item.attr("data-" + defaultOptions.namespace + "-default");
 				
 				if(item.val() == valDef)
-					isValid	= false;	
+				{
+					isValid	= false;
+					errorsCollection.push({
+						field	: item,
+						message	: defaultOptions.errorMessages.defaultOption,
+						error	: $.ambarFormsValidator.Validator.SELECT_DEFAULT
+					});	
+				}
 			});
 			
+			// Controla un campo a partir de una expresión regular
 			form.find("[data-" + defaultOptions.namespace + "-format]").each(function(index, item){
 				var item	= $(item);
 				var exp		= item.attr("data-" + defaultOptions.namespace + "-format");
 				var regex	= defaultOptions.expresions[exp];
 				
 				if(!regex.test(item.val()))
+				{
 					isValid	= false;
+					errorsCollection.push({
+						field	: item,
+						message	: defaultOptions.errorMessages.format,
+						error	: $.ambarFormsValidator.Validator.FORMAT
+					});	
+				}
 			});
 			
 			return isValid;
@@ -125,10 +197,73 @@
 				}	
 			}
 			else
-			{
-				//TODO : Armar la lógica para que el desarrollador pueda configurar la forma de mostrar los mensajes al usuario.
-				console.log("Hacer algo para que se pueda informar al usuario del resultado");
+			{				
+				switch(defaultOptions.errorDisplay)
+				{
+					case $.ambarFormsValidator.Display.INSIDE			:
+						methods.display_inside();
+						break;
+					case $.ambarFormsValidator.Display.OUTSIDE			:
+						methods.display_outside();
+						break;
+					case $.ambarFormsValidator.Display.FIELDS_BEFORE	:
+						methods.display_before();
+						break;
+					case $.ambarFormsValidator.Display.FIELDS_AFTER		:
+						methods.display_after();
+						break;
+				}
 			}
+		},
+		display_inside	: function() {
+			$(errorsCollection).each(function(index, item){
+				item.field.addClass(defaultOptions.errorClass);
+				var msg	= defaultOptions.errorMessages[item.error];
+				
+				switch(item.error)
+				{
+					case $.ambarFormsValidator.Validator.LENGTH_MIN:
+						var len	= item.field.attr("data-" + defaultOptions.namespace + "-length-min");
+						
+						if(len == 1)
+							len = len + " caracter";
+						else
+							len = len + " caracteres";
+						
+						msg	= msg.replace("{0}", len);
+						break;
+					case $.ambarFormsValidator.Validator.LENGTH_MAX:
+						var len	= item.field.attr("data-" + defaultOptions.namespace + "-length-max");
+						
+						if(len == 1)
+							len = len + " caracter";
+						else
+							len = len + " caracteres";
+						
+						msg	= msg.replace("{0}", len);
+						break;
+					case $.ambarFormsValidator.Validator.EQUAL_TO:
+						var parent	= item.field.attr("data-" + defaultOptions.namespace + "-equal-to");
+						
+						msg	= msg.replace("{0}", $('label[for="' + parent + '"]').html());
+						msg	= msg.replace("{1}", $('label[for="' + item.field.attr("name") + '"]').html());
+						break;
+				}
+				
+				if(item.field.attr("title") == undefined)
+					item.field.attr("title", msg);
+				else
+					item.field.attr("title", item.field.attr("title") + "\n" + msg);
+			});
+		},
+		display_outside	: function() {
+			console.log("V0.2 - Muestra los mensajes de error fuera del campo a través de un elemento span");
+		},
+		fields_before	: function() {
+			console.log("V0.3 - Muestra los mensajes de error todos juntos al principio del formulario en un elemento div");
+		},
+		fields_after	: function() {
+			console.log("V0.3 - Muestra los mensajes de error todos juntos al final del formulario en un elemento div");
 		}
 	};
 
@@ -141,8 +276,26 @@
 		} else {
 			$.error('Method ' + method + ' does not exist on jQuery.ambarFormsValidator');
 		}
-
 	};
-
+	
+	// Define las constantes de las validaciones a nivel global
+	$.ambarFormsValidator	= {
+		Validator	: {
+			REQUIRED		: "required",
+			LENGTH_MIN		: "minLength",
+			LENGTH_MAX		: "maxLength",
+			EQUAL_TO		: "equalTo",
+			SELECT_DEFAULT	: "default",
+			FORMAT			: "format"
+		},
+		Display		: {
+			INSIDE			: "inside",
+			OUTSIDE			: "outside",
+			FIELDS_BEFORE	: "before",
+			FIELDS_AFTER	: "after"
+		},
+		version		: "0.1.0"
+	} 
+	
 })(jQuery);
 $(document).ambarFormsValidator();
